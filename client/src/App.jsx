61afedfddcd0cacc1607e291dd50eb6309b42e5e
mich0rtega <156623518+mich0rtega@users.noreply.github.com>
2024-09-18 11:22:24 -0600

    PROJECT |  FIXES BUG

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import react from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
+
 
 export function App() {
   function Logout() {
