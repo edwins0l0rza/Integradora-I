4072248063b3d773c3f514f92aeb97fa647e828e
mich0rtega <156623518+mich0rtega@users.noreply.github.com>
2024-10-02 19:54:34 -0600

    BACKEND | Modelos\Producto\Categoria

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#tabla categoria
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=60)
+    referencia = models.IntegerField
+#Tabla productos
+class Productos(models.Model):
+    nombre = models.CharField(max_length=60)
+    descripcion = models.CharField(max_length=60)
+    modelo = models.CharField(max_length=25)
+    referencia = models.IntegerField(max_length=8)
+    marca = models.CharField(max_length=60)
+    cantidad = models.DecimalField
+    precio = models.DecimalField
+#llave foranea
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
